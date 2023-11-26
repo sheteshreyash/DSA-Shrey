086285b72e6d6ee8818ecb9816ca6054fa9b5d3e
sheteshreyash <72278490+sheteshreyash@users.noreply.github.com>
2023-11-26 14:01:33 +0530

    new changes in LL

---
+++ DSA Codes Shrey/Linked List/LinkedList1.c
@@ -0,0 +1,93 @@
+// Creation and Traversal of Linked List
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+void listTraversal(struct Node *ptr) {
+    while (ptr != NULL)
+    {
+        printf("Element : %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
+    //Implementation 1 :- Using Hardcoded values 
+    // struct Node *head;
+    // struct Node *second;
+    // struct Node *third;
+
+    //Dynamically allocate memory for nodes in the linked list in heap
+    // head = (struct Node *)malloc(sizeof(struct Node));
+    // second = (struct Node *)malloc(sizeof(struct Node));
+    // third = (struct Node *)malloc(sizeof(struct Node));
+
+    //Linking the nodes  
+    // head->data = 7;    
+    // head->next = second;   //link first and second node
+    
+    // second->data = 78;    
+    // second->next = third;   //link second and third node
+
+    // third->data = 88;    
+    // third->next = NULL;   //Terminate the list at last (third) node
+
+    // listTraversal(head); // Head is a pointer of struct node* type
+
+
+
+
+    // Implementation 2 :- Using values for user 
+    struct Node *head = NULL;
+    struct Node *current = NULL;
+    int n; // Number of nodes
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i)      // Input data for each node
+    {
+        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+        if (!newNode)
+        {
+            printf("Memory allocation failed !");
+            return 1;
+        }
+
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (!head)
+        {
+            head = newNode;               // If it's the first node, set it as the head
+            current = newNode;
+        }
+        else
+        {
+            current->next = newNode;               // Link the current node to the new node
+            current = newNode;               // Move the current pointer to the new node
+        }
+    }
+
+    printf("Linked List:\n");       // Traverse and print the linked list
+    listTraversal(head);
+
+    // Free the allocated memory
+    struct Node *temp;
+    while (head)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+    return 0;
+}
+
