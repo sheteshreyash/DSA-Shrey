c5c11e7a6ba71c74f7c614100c987258b54d4eaf
sheteshreyash <72278490+sheteshreyash@users.noreply.github.com>
2023-11-27 20:06:18 +0530

    circular LL

---
+++ DSA Codes Shrey/Linked List/4_CircularLinkedList.c
@@ -0,0 +1,116 @@
+// Creation and Traversal of a Doubly Linked List
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;  //Self referencing structure
+};
+
+void listTraversalDoublylist(struct Node *head) {  //function for traversal of doubly linked list
+
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node *ptr = head;
+    do
+    {
+        printf("Element is : %d\n", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+    
+}
+
+void deallocateMemory(struct Node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    struct Node *current = head;
+    struct Node *nextNode;
+    do
+    {
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    } while (current != head);
+}
+
+int main()
+{
+   // Implementation 1 :- Using Hardcoded values 
+    // struct Node *head;
+    // struct Node *second;
+    // struct Node *third;
+
+    // //Dynamically allocate memory for nodes in the linked list in heap
+    // head = (struct Node *)malloc(sizeof(struct Node));
+    // second = (struct Node *)malloc(sizeof(struct Node));
+    // third = (struct Node *)malloc(sizeof(struct Node));
+
+    // //Linking the nodes  
+    // head->data = 7;    
+    // head->next = second;   //link first and second node
+    
+    // second->data = 78;    
+    // second->next = third;   //link second and third node
+
+    // third->data = 88;    
+    // third->next = head;   //Terminate the list at last (third) node
+
+    // listTraversalDoublylist(head); // Head is a pointer of struct node* type
+
+
+
+    // Implementation 2: - Creating doubly linked list using dynamic allocation
+    int size;
+    printf("Enter the size of the circular linked list: ");
+    scanf("%d", &size);
+
+    if (size <= 0)
+    {
+        printf("Invalid size.\n");
+        return 1;
+    }
+
+    struct Node *head = NULL;
+    struct Node *prevNode = NULL;
+
+    for (int i = 0; i < size; ++i)
+    {
+        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        printf("Enter the value for node %d: ", i);
+        scanf("%d", &(newNode->data));
+
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            prevNode->next = newNode;
+        }
+        prevNode = newNode;
+    }
+
+    if (head != NULL)     // Linking the last node to the head to make it circular (imp step)
+    {
+        prevNode->next = head;
+    }
+
+    printf("\nCircular Linked List created successfully.\n");
+    listTraversalDoublylist(head);
+
+    deallocateMemory(head);
+    return 0;
+}
+
---
+++ DSA Codes Shrey/Linked List/5_InsertionCircularList.c
@@ -0,0 +1,114 @@
+// Types of Insertion in a Doubly Linked List
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;  //Self referencing structure
+};
+
+void listTraversalDoublylist(struct Node *head) {  //function for traversal of doubly linked list
+
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node *ptr = head;
+    do
+    {
+        printf("Element is : %d\n", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+    
+}
+
+struct Node *insertAtFirst(struct Node *head, int data) {  // Case 1
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    struct Node *p = head->next;
+    while (p->next != head)
+    {
+        p = p->next;
+    }                 //At this moment, p points to the last node of the circular linked list
+    p->next = ptr;
+    ptr->next = head;
+    head = ptr;
+    return head;
+}
+void deallocateMemory(struct Node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    struct Node *current = head;
+    struct Node *nextNode;
+    do
+    {
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    } while (current != head);
+}
+
+int main()
+{
+    int size;
+    printf("Enter the size of the circular linked list: ");
+    scanf("%d", &size);
+
+    if (size <= 0)
+    {
+        printf("Invalid size.\n");
+        return 1;
+    }
+
+    struct Node *head = NULL;
+    struct Node *prevNode = NULL;
+
+    for (int i = 0; i < size; ++i)
+    {
+        struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        printf("Enter the value for node %d: ", i);
+        scanf("%d", &(newNode->data));
+
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            prevNode->next = newNode;
+        }
+        prevNode = newNode;
+    }
+
+    if (head != NULL)     // Linking the last node to the head to make it circular (imp step)
+    {
+        prevNode->next = head;
+    }
+
+    printf("\nCircular Linked List created successfully.\n");
+    listTraversalDoublylist(head);
+
+    //Case 1 :- Element to be added at the Beginning/first Node 
+    int val;
+    printf("\n1) Enter the Node Value to be added at Beginning of List : ");
+    scanf("%d", &val);
+    head = insertAtFirst(head, val);// new element that to be updated at first node
+    printf("\nUpdated Circular Linked List after inserting a new node at beginning:\n");
+    listTraversalDoublylist(head);
+    //
+
+    deallocateMemory(head);
+    return 0;
+}
+
