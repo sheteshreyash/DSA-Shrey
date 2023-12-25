56001e1b0184ed9b024a31453d1c00b41eb1ff99
sheteshreyash <72278490+sheteshreyash@users.noreply.github.com>
2023-12-25 20:01:29 +0530

    Insertion sort completed

--- DSA Codes Shrey/7_Sorting Algorithms/7_2_InsertionSort.c
+++ DSA Codes Shrey/7_Sorting Algorithms/7_2_InsertionSort.c
@@ -1,7 +1,6 @@
 // C code for Insertion Sort 
 
 #include <stdio.h>
-#include<stdio.h>
 
 void printArray(int* A, int n){
     for (int i = 0; i < n; i++)
@@ -11,7 +10,7 @@ void printArray(int* A, int n){
     printf("\n");
 }
 
-void insertionSort(int *A, int n){
+void insertionSort(int *A, int n){  // Insertion Sort Function
     int key, j;
     // Loop for passes
     for (int i = 1; i <= n-1; i++)
@@ -27,8 +26,29 @@ void insertionSort(int *A, int n){
     }
 }
 
-int main(){
-   // -1   0    1   2   3   4   5
+
+int main() {
+    int choice;
+    printf("\nInsertion Sort Algorithm \n");
+    printf("1. Insertion Sort..\n");
+    printf("\nEnter your choice : ");
+    scanf("%d", &choice);
+
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    // Logic 1 :- Array size is fixed and have to fill the array completely
+    int A[n];   
+    printf("Enter %d elements of the array:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+
+    printf("\nArray before sorting:\n");
+    printArray(A, n);
+
+// -1   0    1   2   3   4   5
    //      12,| 54, 65, 07, 23, 09 --> i=1, key=54, j=0
    //      12,| 54, 65, 07, 23, 09 --> 1st pass done (i=1)!
 
@@ -48,10 +68,14 @@ int main(){
    //      07, 12, 23, 54, 65,| 09 --> i=5, key=09, j=4
    //      07, 09, 12, 23, 54, 65| --> After the 5th pass 
     
-    int A[] = {12, 54, 65, 7, 23, 9};
-    int n = 6;
-    printArray(A, n);
-    insertionSort(A, n);
+
+    if (choice == 1) {
+        insertionSort(A, n);
+        printf("Array after Bubble Sort:\n");
+    } else {
+        printf("Invalid choice\n");
+        return 1; // Exit with an error code
+    }
     printArray(A, n);
     return 0;
 }
